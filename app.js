9221c084cb2930ff638bce0b9adc835c76055eca
alex <observable@163.com>
2016-08-23 22:33:39 +0800

    -更新

---
+++ app.js
@@ -0,0 +1,38 @@
+
+
+var express = require('express');
+var Mock 	= require('mockjs');
+var app 	= express();
+
+
+
+var router 	= require('./router');
+
+doRouter(router.router);
+
+function doRouter(r) {
+	for (var k in r) {
+		var data = Mock.mock(r[k]);
+		(function (d) {
+			app.get(k, function (req, res) {
+				res.send(d);
+			});
+		})(data);
+	}
+}
+
+
+
+app.use(express.static('./'));
+
+app.get('/test', function (req, res) {
+	res.sendfile('./t4.html');
+});
+
+var server = app.listen(3000, function (a, b) {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Example app listening at http://%s:%s', host, port);
+});
+
