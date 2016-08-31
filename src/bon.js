58fe6a037c83553d5c2c336c1049abfbb3ef0346
sun <sunsun@126.com>
2016-08-31 22:05:23 +0800

    -

--- src/bon.js
+++ src/bon.js
@@ -11,8 +11,8 @@
 var bon = function() {
 	
 	var customTagsReg     	= /<\/?(?:each|if)\s?[^>]*>/g;
-	var eachAttributeReg   	= /<each\s+([\w.]+)\=['"]?(\w+)['"]?(?:\s+([\w.]+)\=['"]?(\w+)['"]?)*\s*>/g;
 	var ifAttributeReg    	= /<if\s+([^>]+)\s*>/g;
+	var eachAttributeReg   	= /<each\s+([\w.]+)\=['"]?(\w+)['"]?(?:\s+([\w.]+)\=['"]?(\w+)['"]?)*\s*>/g;
 	
 	var isES5 = !!Object.defineProperty;
 	var cache = {};
@@ -20,10 +20,10 @@ var bon = function() {
 	return {
 		render: function(data, rawHtml) {
 			if(cache[rawHtml]) {
-				return cache[rawHtml].call(this, data);
+				return cache[rawHtml].call(data);
 			}
 			this.complier(rawHtml);			
-			return cache[rawHtml].call(this, data);
+			return cache[rawHtml].call(data);
 		},
 		complier: function(rawHtml) {
 			
@@ -47,31 +47,38 @@ var bon = function() {
 						}
 						
 						// 如果没有.符号，判断key前缀（如果有）或key在当前环境是否存在，如不存在，取根级的，加前缀data
-						return ["' + ", (fn || ""), "(typeof ", k, " == \"undefined\" || ", k, " == \"\" ? rootData.", k, " : ", k, ") + '"].join('');
+						return ["' + ", (fn || ""), "(typeof ", k, " == \"undefined\" || ", k, " == \"\" ? this.", k, " : ", k, ") + '"].join('');
 					});
 					isES5 ? statement += "compilerTpl += ('" + hTag + "'); \n" : statement.push("compilerTpl.push('" + hTag + "'); \n");
 				}
 				if(customTags && customTags[i]) {
 					var cTag = customTags[i];
-					cTag = cTag.replace(eachAttributeReg, function(x, arrVar, itemVar, countVar, indexVar) {
-						return 'var arr = typeof ' + arrVar + ' == "undefined" ? rootData.' + arrVar + ' : ' + arrVar + '; \narr.forEach(function(' + itemVar + ', ' + indexVar + ') { \n';
+					cTag = cTag.replace(eachAttributeReg, function(x, arr, item, count, index) {
+						var id 			= 'v_' + Math.random().toString(36).slice(2, 8);
+						var arrVar 		= id + '_arr';
+						var itemVar 	= id + '_' + item + '_it';
+						var indexVar 	= id + '_i';
+						return 'var ' + arrVar + ' = typeof ' + arr + ' == "undefined" ? this.' + arr + ' : ' + arr + '; \n' + 
+						'for(var ' + indexVar + ' = 0; ' + indexVar + ' < ' + arrVar + '.length; ' + indexVar + ' ++) {\n' +
+						'	var ' + item + ' = ' + arrVar + '[' + indexVar + '];\n';
+						
 					});
 					
 					cTag = cTag.replace(ifAttributeReg, function(x, ifExpression) {
 					
 						// 判断key前缀（如果有）或key在当前环境是否存在，如不存在，取根级的，加前缀data
 						ifExpression = ifExpression.replace(/\b(?:([\w]+)[\w\[\]]*)(?:\.\w+)?\b(?!['".])/g, function(x, a) {
-							return '(typeof ' + a + ' == "undefined" || ' + a + ' == "" ? rootData.' + x + ' : ' + x + ')';
+							return '(typeof ' + a + ' == "undefined" || ' + a + ' == "" ? this.' + x + ' : ' + x + ')';
 						});
 						
 						return 'if(' + ifExpression + ') {\n';
-					}).replace(/<\/if>/g, '}\n').replace(/<\/each>/g, '});\n');
+					}).replace(/<\/if>/g, '}\n').replace(/<\/each>/g, '}\n');
 					
 					isES5 ? statement += cTag : statement.push(cTag);
 				}
 			}
 			statement = isES5 ? statement : statement.join('');
-			cache[rawHtml] = new Function('rootData', statementHeader + statement + statementFooter);
+			cache[rawHtml] = new Function(statementHeader + statement + statementFooter);
 			statement = statementHeader = statementFooter = customTags = htmlTags = null;
 		}
 	}
