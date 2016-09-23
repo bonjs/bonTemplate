f8f875c3f1e6a1373fdca13011be7e708dedc989
alex <observable@163.com>
2016-09-23 21:10:11 +0800

    d

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var Mock 	= require('mockjs');
+var app 	= express();
+
+
+app.use(express.static('./publish'));
+
+app.get('/test', function (req, res) {
+	res.sendfile('./t3.html');
+});
+
+var server = app.listen(3000, function (a, b) {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log(' listening at http://%s:%s', host, port);
+});
+
