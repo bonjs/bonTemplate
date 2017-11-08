ee58e9ff7e7f28d0aef022791e9c28944e3d2cd5
alex <observable@163.com>
2017-11-08 17:53:58 +0800

    -增加表达式中对点的处理

--- router.js
+++ router.js
@@ -15,6 +15,29 @@ var router = {
 		'id': /\w{32}/,
         'name': /[a-z]{3,6}/
 	},
+	'/data': {
+		'list|100': [
+			{
+				'index|+1': 1,
+				user: '<strong style="color:red">ÌÇ±ý</strong>',
+				site: 'http://www.planeart.cn',
+				weibo: 'http://weibo.com/planeart',
+				QQweibo: 'http://t.qq.com/tangbin'
+			}
+		]
+	},
+	'/data2': {
+		name:'¹þ¹þ',
+		'list|100': [
+			{
+				'index|+1': 1,
+				user: '<strong style="color:red">ÌÇ±ý</strong>',
+				site: 'http://www.planeart.cn',
+				weibo: 'http://weibo.com/planeart',
+				QQweibo: 'http://t.qq.com/tangbin'
+			}
+		]
+	},
 	'/getData': {
 		title : 'ckk',
 		'departList|2' : [{
@@ -34,9 +57,9 @@ var router = {
 														'list|2': [
 															{name: /\w{5}/,
 																'list|2': [
-																	{name: /\w{5}/,
+																	{name: /\d{2}/,
 																		'list|2': [
-																			{name: /\w{5}/}
+																			{name: /\d{5}/}
 																		]
 																	}
 																]
