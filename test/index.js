76e0cc74a7d47f30d498c24ae5407f1fc861cb3c
Alex <observable@163.com>
2018-12-29 20:39:13 +0800

    mocha

---
+++ test/index.js
@@ -0,0 +1,12 @@
+
+var {stringify} = require("../");
+var assert  = require("assert");
+
+describe('test index.js', function() {
+    it('output should equals ', function() {
+       if(false) {
+         throw new Error("the ouput is not what we expacted !");
+       }
+    });
+});
+
